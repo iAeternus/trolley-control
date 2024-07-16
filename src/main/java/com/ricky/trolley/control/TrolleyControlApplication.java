384ec65fa2770faf403f36ffde8b1c466422ed2a
Ricky <w_ziwei2004@163.com>
2024-07-16 17:49:24 +0800

    init

---
+++ src/main/java/com/ricky/trolley/control/TrolleyControlApplication.java
@@ -0,0 +1,13 @@
+package com.ricky.trolley.control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrolleyControlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrolleyControlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ricky/trolley/control/TrolleyControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricky.trolley.control;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrolleyControlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
